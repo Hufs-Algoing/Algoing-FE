eb51d4cef9a99607c87bbfd120a852b5945773e8
honi31 <winner020531@naver.com>
2025-04-10 15:19:16 +0900

    Initial Setting

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      Algoing Home
+    </div>
+  );
+}
