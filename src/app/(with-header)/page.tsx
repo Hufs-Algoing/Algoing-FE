07c53a3f65aa9a39a335b68c3356f4f8408c4a80
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-04-27 17:27:22 +0900

    feat: home dashboard 초기 구현

--- src/app/(with-header)/page.tsx
+++ src/app/(with-header)/page.tsx
@@ -1,10 +1,9 @@
+import Main from "./main/page";
+
 export default function Home() {
   return (
     <>
-      {" "}
-      <div className="flex w-full h-screen items-center justify-center">
-        임시 홈
-      </div>
+      <Main />
     </>
   );
 }
