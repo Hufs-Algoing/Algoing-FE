356d9ba1c6c79f71e848ee717a7550ef6c3c2d1a
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-06-11 12:50:56 +0900

    fix: 미들웨어 예외처리

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,14 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/((?!login).*)"],
+  matcher: ["/((?!_next|favicon.ico|login).*)"], // 정적 리소스 예외 처리 포함
 };
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("Authorization")?.value;
   const pathname = req.nextUrl.pathname;
-  if (!token && pathname === "/login") {
+
+  if (!token && pathname !== "/login") {
     return NextResponse.redirect(new URL("/login", req.url));
   }
+
   return NextResponse.next();
 }
